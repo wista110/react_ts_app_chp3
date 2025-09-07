5c965c74c1151ad5a22843177ce6ddef1038259d
wista110 <wista.dev1@gmail.com>
2025-09-07 22:52:51 +0900

    コンポーネントについて

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
+// デフォルトのApp.tsx
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// チャプター３ App.tsx
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const title = "React page.";
+const massage = "メッセージを表示します";
+
+function Msg() {
+  return <p className='msg'>Hello!!</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{title}</h1>
+      <h2>{massage}</h2>
+      <Msg />
+      <Msg />
+      <Msg />
     </div>
   );
 }
-
 export default App;
