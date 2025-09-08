33daea475d4b0276b04c178d76930efac3b247da
wista110 <wista.dev1@gmail.com>
2025-09-08 23:10:49 +0900

    JSXの使い方①

--- src/App.tsx
+++ src/App.tsx
@@ -26,39 +26,65 @@
 
 // export default App;
 
-// チャプター３ App.tsx
+// チャプター３-2 App.tsx
+// import React from 'react';
+// import './App.css';
+
+// const title = "React page.";
+// const massage = "メッセージを表示します";
+
+// // MsgPropsインターフェイスの定義
+// interface MsgProps {
+//   msg: string;
+//   size: number;
+//   color: string;
+// }
+
+// function Msg(props: MsgProps) {
+//   const s = {
+//     fontSize: props.size + "pt",
+//     color: props.color,
+//   }
+//   return <p className='msg' style={s}>{props.msg}</p>;
+// }
+
+// function App() {
+//   return (
+//     <div className='container'>
+//       <h1>{title}</h1>
+//       <h2>{massage}</h2>
+//       <div>
+//       <Msg msg="最初のメッセージ" size={20} color="orange" />
+//       <Msg msg="次のメッセージ" size={20} color="lightgreen" />
+//       <Msg msg="最後のメッセージ" size={20} color="skyblue" />
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
+
+// チャプター３-3 App.tsx
 import React from 'react';
 import './App.css';
 
 const title = "React page.";
 const massage = "メッセージを表示します";
+const content = "これがtrueの場合に表示されるメッセージです。";
 
-// MsgPropsインターフェイスの定義
-interface MsgProps {
-  msg: string;
-  size: number;
-  color: string;
-}
-
-function Msg(props: MsgProps) {
-  const s = {
-    fontSize: props.size + "pt",
-    color: props.color,
-  }
-  return <p className='msg' style={s}>{props.msg}</p>;
-}
+const flg = true; //trueの時にメッセージを表示
 
 function App() {
   return (
     <div className='container'>
       <h1>{title}</h1>
-      <h2>{massage}</h2>
-      <div>
-      <Msg msg="最初のメッセージ" size={20} color="orange" />
-      <Msg msg="次のメッセージ" size={20} color="lightgreen" />
-      <Msg msg="最後のメッセージ" size={20} color="skyblue" />
+      <p>{massage}</p>
+      {flg && 
+      <div className='msg'>
+      <p>{content}</p>
       </div>
+      }
     </div>
   );
 }
+
 export default App;
